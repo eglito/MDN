de65adc4ca1d12277a7d46bee0cc52d57c905117
eglito <aleeglito@gmail.com>
2025-01-24 12:02:13 -0300

    commit: "refactored UserMapper class"

--- src/main/java/com/example/project/utils/mapper/UserMapper.java
+++ src/main/java/com/example/project/utils/mapper/UserMapper.java
@@ -1,23 +1,31 @@
 package com.example.project.utils.mapper;
 
+import com.example.project.adapters.outbound.repositories.UserMappe;
 import com.example.project.domain.dto.UserDTO;
 import com.example.project.domain.entity.User;
-import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Component
-public class UserMapper {
+public class UserMapper implements UserMappe {
 
-    public User toEntity(UserDTO userDTO){
+    @Override
+    public UserDTO toUserDTO (User user){
+        if(user == null){
+            return null;
+        }
+        UserDTO userDTO = new UserDTO(user.getEmail(), user.getUuid());
+        return userDTO;
+    }
+
+    @Override
+    public User toUser(UserDTO userDTO){
+        if(userDTO == null){
+            return null;
+        }
         User user = new User();
         user.setEmail(userDTO.getEmail());
         user.setUidd(UUID.randomUUID());
         return user;
     }
 
-    public UserDTO toUserDTO(User user){
-        UserDTO userDTO = new UserDTO(user.getEmail(), user.getUuid());
-        return userDTO;
-    }
 }
