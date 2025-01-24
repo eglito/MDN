f23f551508ae88bb20c08f719b9a1dfafd51a066
eglito <aleeglito@gmail.com>
2025-01-24 11:20:50 -0300

    commit: "creating the UserMapper interface"

---
+++ src/main/java/com/example/project/adapters/outbound/repositories/UserMapper.java
@@ -0,0 +1,12 @@
+package com.example.project.adapters.outbound.repositories;
+
+import com.example.project.domain.dto.UserDTO;
+import com.example.project.domain.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+    UserDTO toUserDTO (User user);
+    User toUser(UserDTO userDTO);
+}
